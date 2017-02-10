6c7aad2de6a15bec78bee31f153be1c78d4bda7b
valdrinkoshi <valdrin@google.com>
2017-02-10 12:36:02 -0800

    use html-imports-native. Fire WebComponentsReady once.

--- src/post-polyfill.js
+++ src/post-polyfill.js
@@ -8,7 +8,7 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-(function(scope) {
+(function() {
 
   'use strict';
 
@@ -38,10 +38,4 @@
 
   }
 
-  HTMLImports.whenReady(function() {
-    requestAnimationFrame(function() {
-      window.dispatchEvent(new CustomEvent('WebComponentsReady'));
-    });
-  });
-
-})(window.WebComponents);
+})();
